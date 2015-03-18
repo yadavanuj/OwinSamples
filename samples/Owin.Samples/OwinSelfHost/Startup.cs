47eaa520bc8ea963b2d38b75545cb62ef129c237
unknown <ayadav@indev116.corp.soti.net>
2015-03-18 18:52:10 +0530

    Initial commit

---
+++ samples/Owin.Samples/OwinSelfHost/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Owin;
+
+namespace OwinSelfHost
+{
+
+
+public class Startup
+{
+    public void Configuration(IAppBuilder app)
+    {
+#if DEBUG
+        app.UseErrorPage();
+#endif
+        app.UseWelcomePage("/");
+    }
+}
+}
